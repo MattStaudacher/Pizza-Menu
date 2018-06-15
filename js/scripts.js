e4663c1885894acf6f0fb5de947bab8a88987fb8
Matt Staudacher <mattstaudacher3@gmail.com>
2018-06-15 14:51:48 -0700

    initialized files,created html layout for pizza menu

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+
+//business logic
+
+function Pizza(size, topping) {
+  this.size = size;
+  this.topping = topping;
+}
+
+Movie.prototype.total = function() {
+
+return
+}
+
+//user interface
+$(document).ready(function() {
+  $("form#form-pizza").submit(function(event) {
+    event.preventDefault();
+
+    Pizza.prototype.total = function() {
+      return this.siz
+    }
+    var selectedSize = $("select#pizza-size").val();
+    var selectedToppings = $("select#movie-titles").val();
+    var total= "";
+  }
+})
