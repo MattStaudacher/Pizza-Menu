65471285425a83befc89e17dc8314288d87cb4eb
Matt Staudacher <mattstaudacher3@gmail.com>
2018-06-26 19:59:42 -0700

    delete toppings menu to simplify function in an attempt to make project work.

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,66 @@
 
 //business logic
 
-function Pizza(size, topping) {
+function Pizza(size, topping, cost) {
   this.size = size;
   this.topping = topping;
+  this.cost = 0;
+
+
 }
 
-Pizza.prototype.total = function() {
+Pizza.prototype.totalCost = function() {
 
-return
-}
+    if(this.toppings<=2 && this.size==="small")
+    {
+        this.price+= 7;
 
-//user interface
-$(document).ready(function() {
-  $("form#form-pizza").submit(function(event) {
-    event.preventDefault();
+      }else if (this.size==="medium") {
+        this.price+= 10;
 
-    Pizza.prototype.total = function() {
-      return this.siz
+      }else if (this.size==="large") {
+        this.price+= 13;
+
+
+    }else if (this.toppings>=3) {
+      if(this.size=="small"){
+        this.price+= 10;
+
+      }else if (this.size=="medium") {
+        this.price+= 12.50;
+
+      }else if (this.size=="large") {
+        this.price+= 15;
+      }
     }
-    var selectedSize = $("select#pizza-size").val();
-    console.log(selectedSize);
-    var selectedToppings = $("select#pizza-toppings").val();
-    console.log(selectedToppings);
-    var total= "";
+
+  }
+var totalCost = 0;
+//user interface
+$(document).ready(function(){
+  $("form#form-pizza").submit(function(event){
+    event.preventDefault();
+    $("#pizza-order").show();
+    var size = $("#pizza-size").val();
+    var toppingsChecked=[];
+    var topping=0;
+    $("input:checkbox[name=toppings]:checked").each(function(){
+      topping += 1;
+      toppingsChecked.push($(this).val());
+    });
+    var newZa= new Pizza(size, topping)
+    newZa.totalCost();
+    totalCost+=newZa.price;
+    // $(".size").text(newPizza.size);
+    $(".totalCost").text(totalCost);
+
+    $("ul#order-total").append("<li>"+"One "+ newZa.size +" "+ "size "+" "+" pizza"+ " with "+ toppingsChecked + " "+": $" +" "+ newZa.cost+ "</li>");
+    toppingsChecked=[];
 
 
-    var smallPrice= 7;
-    var mediumPrice= 10;
-    var largePrice= 13;
+alert();
+    $("#pizza-order").trigger("reset");
+
+
   });
 });
